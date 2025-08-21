5ed039093ab7fa6b6552cffca05a2043f70ec792
Denat M <ddntii12@gmail.com>
2025-08-21 15:00:14 -0500

    Update main.go

--- main.go
+++ main.go
@@ -12,3 +12,36 @@ import (
 "time"
 )
 
+type URLEntry struct {
+	URL       string    `json:"url"`
+	CreatedAt time.Time `json:"created_at"`
+	Clicks    int       `json:"clicks"`
+}
+
+type Store struct {
+    Items map[string]URLEntry 'json."items"'
+}
+
+func dbPath() string {
+    return filepath.Join(".", "urls.json")
+}
+
+func load() Store {
+    var s Store
+    s.Items = map[string]URLEntry{}
+    data, err := os.ReadFile(dbPath())
+    if err != nil {
+        return s 
+    }
+    if err :=json.Unmarshal(data, &s); err != nil {
+    return Store{Items: map[string]URLEntry{}}
+    }
+    if s.Items == nil {
+        s.Items = map[string]URLEntry{}
+    }
+    return s 
+}
+
+const alphabet 
+
+
